ed145d5aa560aab4361c1d2724a25c1840084429
kebertx <kebertyx@gmail.com>
2015-02-09 14:17:47 -0700

    Changed most things! Commit often? One day I'll be a good christian...

--- Markov.hs
+++ Markov.hs
@@ -6,6 +6,19 @@ import Control.Monad.State
 import qualified Data.Map as Map
 
 type MarkovMap = Map.Map String [String]
+type MarkovState = ([String], StdGen)
+type MarkovSt = State MarkovState String
+
+
+toSentence :: [String] -> String
+toSentence ls = foldl join' "" (reverse ls)
+     where join' [] [] = ""
+           join' [] (s:ss) = toUpper s : ss
+           join' a b = a ++ " " ++ b
+
+showMarkovState :: MarkovState -> String
+showMarkovState = toSentence . fst
+
 
 chain :: String -> [String]
 chain line = filter (not . null) $ map clean (words line)
@@ -16,41 +29,50 @@ insertChain [] mp = mp
 insertChain [x] mp = mp
 insertChain (x:xs) mp = insertChain xs $ Map.insertWith (++) x [head xs] mp
 
-parseFile' :: MarkovMap -> Handle -> IO MarkovMap
-parseFile' mp handle = do
-  eof <- hIsEOF handle
-  if not eof
-    then do line <- hGetLine handle
-            parseFile' (insertChain (chain line) mp) handle
-    else return mp
 
 parseFile :: String -> IO MarkovMap
 parseFile fname = openFile fname ReadMode >>= parseFile' Map.empty 
+    where parseFile' :: MarkovMap -> Handle -> IO MarkovMap
+          parseFile' mp handle = do
+            eof <- hIsEOF handle
+            if not eof
+            then do line <- hGetLine handle
+                    parseFile' (insertChain (chain line) mp) handle
+            else return mp
 
-randomRSt :: Int -> State StdGen Int
-randomRSt num = state (randomR (0, num - 1))
 
-randomIdxSt :: [a] -> State StdGen Int
-randomIdxSt ls = randomRSt $ length ls
+addToChain :: MarkovMap -> MarkovState -> (String, MarkovState)
+addToChain mp ([], g) = let (idx, ng) = randomR (0, Map.size mp - 1) g
+                            start = fst $ Map.elemAt idx mp
+                        in (start, ([start], ng))
+addToChain mp (ls, g) = let choices = ((Map.!) mp (head ls))
+                            (idx, ng) = randomR (0, length choices - 1) g
+                            word = choices !! idx
+                        in (word, (word:ls, ng))
 
+addToChainSt :: MarkovMap -> MarkovSt
+addToChainSt mp = state (addToChain mp)
+
+passChain :: MarkovSt
+passChain = do
+  ((x:_),g) <- get
+  put ([x],g)
+  return x
+
+emptyChain :: IO MarkovState
+emptyChain = do
+  g <- getStdGen
+  return ([], g)
+                           
 main :: IO ()
 main = do
   args <- getArgs
   mp <- parseFile (head args)
+  start <- emptyChain
 
-  g <- getStdGen
-  let st = randomRSt (Map.size mp)
-      (idx, ng) = runState st g
+  let atc = addToChainSt mp
+      ret = snd $ runState (replicateM 12 atc) start
+      mrkv = snd $ runState (passChain >> replicateM 12 atc) ret
+  putStrLn . showMarkovState $ ret
+  putStrLn . showMarkovState $ mrkv
   
-  -- This would be prettier if it were Monady...
-  let start = fst $ Map.elemAt idx mp
-      (idx', ng') = runState (randomIdxSt ((Map.!) mp start)) ng
-
-      mid = (((Map.!) mp start) !! idx')
-      idx'' = fst $ runState (randomIdxSt ((Map.!) mp mid)) ng'
-
-      end = (((Map.!) mp mid) !! idx'')
-
-  print start
-  print mid
-  print end
