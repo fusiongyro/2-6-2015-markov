3098e3cb569762a31d5c274480abbbb63f6ba904
kebertx <kebertyx@gmail.com>
2015-02-09 18:49:13 -0700

    Commented functions

--- Markov.hs
+++ Markov.hs
@@ -10,34 +10,41 @@ type MarkovState = ([String], StdGen)
 type MarkovSt = State MarkovState String
 
 
+-- Turns a line of text into a list of strings containing only lowercase letters
 toChain :: String -> [String]
 toChain line = filter (not . null) $ map clean (words line)
   where clean = ((map toLower) . (filter isAlpha))
 
 
+-- Maps every word in a chain to a list of words that have followed it
 insertChain :: [String] -> MarkovMap -> MarkovMap 
 insertChain [] mp = mp
 insertChain [x] mp = mp
 insertChain (x:xs) mp = insertChain xs $ Map.insertWith (++) x [head xs] mp
 
 
--- insertLine :: String -> MarkovMap -> MarkovMap
--- insertLine line = insertChain (toChain line)
+-- Converts a line of text to a chain moves the words into a map
+insertLine :: String -> MarkovMap -> MarkovMap
+insertLine = insertChain . toChain
 
 
+-- Inserts every line of an open file into a map
 markovMapFile :: MarkovMap -> Handle -> IO MarkovMap
-markovMapFile mp handle = do
-  eof <- hIsEOF handle
+markovMapFile mp file = do
+  eof <- hIsEOF file
   if not eof
-  then do line <- hGetLine handle
-          markovMapFile (insertChain (toChain line) mp) handle
+  then do line <- hGetLine file
+          markovMapFile (insertLine line mp) file
   else return mp
 
 
+-- Opens the given filename and inserts the lines of the file into a new map
 parseFile :: String -> IO MarkovMap
 parseFile fname = openFile fname ReadMode >>= markovMapFile Map.empty 
 
 
+-- Takes a Markov chain, randomly chooses a word which might follow the current
+-- word, and returns that word coupled with the new state of the markov chain
 addToChain :: MarkovMap -> MarkovState -> (String, MarkovState)
 addToChain mp ([], g) = let (idx, ng) = randomR (0, Map.size mp - 1) g
                             start = fst $ Map.elemAt idx mp
@@ -48,20 +55,23 @@ addToChain mp (ls, g) = let choices = ((Map.!) mp (head ls))
                         in (word, (word:ls, ng))
 
 
+-- Wraps the addToChain function in a Stateful Computation, so you can do
+-- Monadic things with it!
 addToChainSt :: MarkovMap -> MarkovSt
-addToChainSt mp = state (addToChain mp)
+addToChainSt = state . addToChain
 
 
+-- A Markov chain with no words and a new random number generator
 initMarkovState :: IO MarkovState
 initMarkovState = do
   g <- getStdGen
   return ([], g)
 
 
+-- Converts the state of a Markov chain to a readable sentence
 showMarkovState :: MarkovState -> String
 showMarkovState = toSentence . fst
-    where -- toSentence :: [String] -> String
-          toSentence ls = foldl join' "" (reverse ls)
+    where toSentence ls = foldl join' "" (reverse ls)
               where join' [] [] = ""
                     join' [] (s:ss) = toUpper s : ss
                     join' a b = a ++ " " ++ b
@@ -85,23 +95,25 @@ repeatMarkovSt num st = do
     return $ head ls
 
 
-runMarkovState :: MarkovSt -> MarkovState -> MarkovState
-runMarkovState st start = snd $ runState st start
+-- Like runState but only returns the state, not the result
+runMarkovSt :: MarkovSt -> MarkovState -> MarkovState
+runMarkovSt st start = snd $ runState st start
 
 
-passState :: Int -> MarkovState -> MarkovSt -> MarkovState
-passState len start st = runMarkovState (repeatMarkovSt len st) start
-
+-- passState :: Int -> MarkovState -> MarkovSt -> MarkovState
+-- passState len start st = runMarkovSt (repeatMarkovSt len st) start
+                                    
 
 main :: IO ()
 main = do
   args <- getArgs
   maps <- mapM parseFile args
-  e <- initMarkovState
+  empty <- initMarkovState
 
   let bigmap = foldl (Map.unionWith (++)) Map.empty maps
-      atc = addToChainSt bigmap
-      states = scanl (passState 12) e (replicate 5 atc)
+      append = addToChainSt bigmap
+      eight start st = runMarkovSt (repeatMarkovSt 8 st) start
+      states = scanl eight empty (replicate 10 append)
 
   mapM_ (putStrLn . showMarkovState) states
   
